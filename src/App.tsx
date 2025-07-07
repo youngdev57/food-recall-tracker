fb0d0c642cf0992a2cef6aaa90e27cf27ccb339c
Minyoung Choi <youngdev57@gmail.com>
2025-07-07 18:47:40 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-center">React Project</h1>
+      <h2 className="text-2xl text-center">food-recall-tracker</h2>
+    </div>
+  );
+}
+
+export default App;
