803d161e2d23d54e62be3937cf53652ef74ae70d
Minyoung Choi <youngdev57@gmail.com>
2025-07-29 19:26:50 +0900

    refactor: Prop 선언 타입 변경 (interface to type)

--- src/types/recall.ts
+++ src/types/recall.ts
@@ -1,17 +1,17 @@
-export interface RawRecallItem {
+export type RawRecallItem = {
   RTRVLDSUSE_SEQ: string; // 회수/판매중지 일련번호
   PRDTNM: string; // 제품명
   BSSHNM: string; // 제조업체명
   RTRVLPRVNS: string; // 회수사유
   IMG_FILE_PATH: string; // 제품사진URL
   DISTBTMLMT: string; // 유통/소비기한
-}
+};
 
-export interface RecallItem {
+export type RecallItem = {
   recallSeq: string;
   productName: string;
   brandName: string;
   recallReason: string;
   imageUrl?: string;
   distLimitDate: string;
-}
+};
