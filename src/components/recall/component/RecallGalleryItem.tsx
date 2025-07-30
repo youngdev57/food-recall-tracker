ba379cafef802fd9a9ba70c7b6e367c34c1a2f45
Minyoung Choi <youngdev57@gmail.com>
2025-07-30 20:09:04 +0900

    feat: 이미지 다중 url 수정

--- src/components/recall/component/RecallGalleryItem.tsx
+++ src/components/recall/component/RecallGalleryItem.tsx
@@ -8,7 +8,13 @@ export default function RecallGalleryItem({ item }: Props) {
   return (
     <div className="border rounded p-4 hover:bg-gray-50 transition h-60">
       <div className="w-full h-40 overflow-hidden rounded">
-        <img src={item.imageUrl} alt={item.productName} />
+        {item.imageUrl?.split(", ").map((url) => (
+          <img
+            src={url}
+            alt={item.productName}
+            className="w-full h-full object-cover object-center"
+          />
+        ))}
       </div>
       <div className="font-semibold pt-2">{item.productName}</div>
       <div className="text-sm text-gray-400">{item.brandName}</div>
